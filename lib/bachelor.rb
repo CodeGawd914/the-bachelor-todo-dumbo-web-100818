644324aaf60bf5518ea6e48461d3d2784d3297bb
Travis Desir <travisgdesir31@gmail.com>
2018-09-24 13:00:09 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -4,6 +4,9 @@ def get_first_name_of_season_winner(data, season)
       if y == "Winner"
         winner = people["name"]
         return winner
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
